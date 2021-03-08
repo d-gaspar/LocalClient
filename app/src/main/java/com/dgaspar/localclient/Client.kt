842bd8b234ccb557c94a4be2f0804a3e60ff1381
Daniel Gaspar Gonçalves <d-gaspar@users.noreply.github.com>
2021-03-07 22:42:30 -0300

    basic working socket (send W or S to local IP)

---
+++ app/src/main/java/com/dgaspar/localclient/Client.kt
@@ -0,0 +1,47 @@
+package com.dgaspar.localclient
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.IOException
+import java.io.OutputStream
+import java.net.Socket
+import java.nio.charset.Charset
+import java.util.*
+
+class Client {
+    private var server : Socket? = null
+    private var connected : Boolean = false
+    private var host : String = "localhost"
+    private var port : Int = 8080
+
+    constructor(host : String = "localhost", port : Int = 8080) {
+        this.host = host
+        this.port = port
+    }
+
+    fun run(key : String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                server = Socket(host, port)
+                connected = true
+                println("Connected on $host:${port.toString()}")
+
+                var reader : Scanner = Scanner(server!!.getInputStream())
+                var writer : OutputStream = server!!.getOutputStream()
+
+                while(connected) {
+                    connected = false
+
+                    val input = readLine() ?: ""
+                    writer.write("{\"key\"}:{\"$key\"}".toByteArray(Charset.defaultCharset()))
+
+                    reader.close()
+                    server?.close()
+                }
+            } catch (e : IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
